fe896887cfe9a52b877bf96da1a51ec978e57bfd
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2023-10-11 15:49:48 -0300

    all media

--- src/Data/List.tsx
+++ src/Data/List.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { Carousel } from "antd";
+import { useWindowSize } from '../components/useScreenSize';
+
 
 interface CardProps {
   data: any[];
 }
 
 function List({ data }: CardProps) {
+
+  const size = useWindowSize();
+  const getSize =() =>{
+
+    if (
+      size.width > 1440
+    )
+    {return 3}
+    if (
+      size.width > 1024
+    )
+    {return 2}
+    if(
+      size.width > 768
+    )
+    {return 2}
+    if(
+      size.width > 425
+    )
+    {return 1}
+    return 1
+  }
   return (
     <div>
       <Carousel
@@ -13,7 +37,7 @@ function List({ data }: CardProps) {
         swipeToSlide
         draggable
         autoplay
-        slidesToShow={3}
+        slidesToShow={getSize()}
         autoplaySpeed={4000}
         pauseOnHover={true}
       >
