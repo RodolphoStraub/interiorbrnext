72176fb0745aeae637b0041f989f46e82f58c15a
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-04 19:00:52 -0300

    correções

--- src/components/useScreenSize.tsx
+++ src/components/useScreenSize.tsx
@@ -4,14 +4,14 @@ import { useState, useEffect } from 'react';
 // Hook
 export function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {  
     function handleResize() {
       setWindowSize({
-        width : window.innerWidth,
+        width: window.innerWidth,
         height: window.innerHeight,
       });
     }
